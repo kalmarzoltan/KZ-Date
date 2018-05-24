8a1c8d6fb6e4bf5fc36e9e0e30934b91cd3138ae
Gabor Koncsar <gabor.koncsar@gmail.com>
2018-05-24 20:49:14 +0200

    Date, Calendar

--- src/date/proba/DateProba.java
+++ src/date/proba/DateProba.java
@@ -1,5 +1,6 @@
 package date.proba;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -11,10 +12,21 @@ public class DateProba {
 		Date date = new Date();
 		Calendar calendar = new GregorianCalendar();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.sss");
-		
+
 		System.out.println(sdf.format(date));
-		
-		
+
+		try {
+
+			Date parseDate = sdf.parse("123 123 123");
+			System.out.println(sdf.format(parseDate));
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.out.println("The date format is NG..");
+
+		}
+
+		System.out.println(sdf.format(calendar.getTime()));
 
 	}
 
